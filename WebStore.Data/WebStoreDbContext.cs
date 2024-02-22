7c34848e952d69007379e738213d573b98f0dbe6
Dragomir Kamberov <dkamberov@hotmail.com>
2024-02-22 12:51:47 +0200

    Add project files.

---
+++ WebStore.Data/WebStoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebStore.Data
+{
+    public class WebStoreDbContext : IdentityDbContext
+    {
+        public WebStoreDbContext(DbContextOptions<WebStoreDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
