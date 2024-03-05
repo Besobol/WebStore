5676c15038b69775c61cc472b9a7a0c5c4eb9853
Dragomir Kamberov <dkamberov@hotmail.com>
2024-03-05 16:04:02 +0200

    Applied migration

--- WebStore.Data/Migrations/20240302085935_CreateIdentitySchema.Designer.cs
+++ WebStore.Data/Migrations/20240302085935_CreateIdentitySchema.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using WebStore.Data;
+using WebStore.Infrastructure.Data;
 
 #nullable disable
 
--- WebStore.Data/Migrations/WebStoreDbContextModelSnapshot.cs
+++ WebStore.Data/Migrations/WebStoreDbContextModelSnapshot.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using WebStore.Data;
+using WebStore.Infrastructure.Data;
 
 #nullable disable
 
