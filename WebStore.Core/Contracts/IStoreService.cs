9c275093a33c77284a6c5bac5491015ff5d8b79f
Dragomir Kamberov <dkamberov@hotmail.com>
2024-03-06 18:04:52 +0200

    Added initial StoreServices

---
+++ WebStore.Core/Contracts/IStoreService.cs
@@ -0,0 +1,10 @@
+﻿using WebStore.Core.Models;
+
+namespace WebStore.Core.Contracts
+{
+    public interface IStoreService
+    {
+        Task<IEnumerable<BookCardModel>> GetBookCardsPageAsync(int page, string query, int pageSize);
+
+    }
+}
---
+++ WebStore.Core/Services/StoreService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+using WebStore.Core.Contracts;
+using WebStore.Core.Models;
+using WebStore.Infrastructure.Data.Common;
+using WebStore.Infrastructure.Data.Models;
+
+namespace WebStore.Core.Services
+{
+    public class StoreService : IStoreService
+    {
+        private readonly IRepository repository;
+
+        public StoreService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<IEnumerable<BookCardModel>> GetBookCardsPageAsync(int page, string query, int pageSize)
+        {
+            return await repository.AllReadOnly<Book>()
+                .Where(b => b.Title.Contains(query))
+                .Skip(page - 1 * pageSize)
+                .Take(pageSize)
+                .Select(b => new BookCardModel()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Autor = string.Join(", ",
+                        b.AuthorsBooks
+                        .Where(ab => ab.BookId == b.Id)
+                        .Select(ab => ab.Author.Name)),
+                    ImgPath = b.ImgPath,
+                    Price = b.Price
+                })
+                .OrderBy(b => b.Id)
+                .ToListAsync();
+        }
+    }
+}
